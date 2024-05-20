105b8ca2b498fc7b04e9d34ad5fddcc5e2206349
Meryem Uygurer <hmuygurer@outlook.com>
2024-05-20 23:40:27 +0300

    update

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 
-function Comment({text, maxLength = 100}) {
+function Comment({text, src, alt, name, maxLength = 100}) {
     const [isExpanded, SetIsExpanded] = useState(false);
 
     const toggleExpand = () => {
@@ -8,12 +8,16 @@ function Comment({text, maxLength = 100}) {
     };
 
     const shouldTruncate = text.length > maxLength;
-    const displayText = isExpanded ? text : text.slice(0,maxLength) + shouldTruncate ? '...' : '';
+    const displayText = isExpanded ? text : shouldTruncate ? text.slice(0, maxLength) + '...' : text;
 
     console.log('displayText:', displayText);  // Debugging için eklendi
 
     return(
         <div className="comment">
+            <div className="row">
+                <img src={src} alt={alt} ></img>
+                <span className="comment-name"> {name} </span>
+            </div>
             <p>{displayText}</p>
             {shouldTruncate && (
                 <button onClick={toggleExpand}>
--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -2,14 +2,14 @@ import React from "react";
 import Comment from "./Comment"
 
 const CommentData = [
-    {'src':'../../public/CommentImages/comment-img-1.png', 'text': 'abcedeedadaslpdldlsadlaplpalpaldadsaldlpasl', 'name': 'adasdaad'}
+    {'src':'../../public/CommentImages/comment-img-1.png', 'text': 'abcolsakxsa xxn*paoxğpsaxşlsax kxopsakxoalxa kxoaspkoxax xoaskpxpalsxaslxğal', 'name': 'name', 'alt': 'abcdef'}
 ]
 
 function CommentsList() {
     return(
         <div className="comments-container">
             {CommentData.map((comment, index) => (
-                <Comment text={comment.text} key={index} />
+                <Comment key={index} text={comment.text} name={comment.name} src={comment.src} alt={comment.alt} />
             ))}
         </div>
     );
